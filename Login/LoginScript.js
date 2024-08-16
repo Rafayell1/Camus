46a56ba4ddd74c8d6eebcc0548affd876c496a6f
Capyvara13 <tiagorafleite@gmail.com>
2024-08-16 12:05:38 -0300

    Del background image and their animation

--- Login/LoginScript.js
+++ Login/LoginScript.js
@@ -1,10 +1,10 @@
 function mostrarSenha() {
-    var senhaInput = document.getElementById("password");
-    var tipoInput = senhaInput.getAttribute("type");
-  
-    if (tipoInput === "password") {
-      senhaInput.setAttribute("type", "text");
-    } else {
-      senhaInput.setAttribute("type", "password");
-    }
+  var senhaInput = document.getElementById("password");
+  var tipoInput = senhaInput.getAttribute("type");
+
+  if (tipoInput === "password") {
+    senhaInput.setAttribute("type", "text");
+  } else {
+    senhaInput.setAttribute("type", "password");
   }
+}
