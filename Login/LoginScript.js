c0b347097997c1fc04f0ba202740aa1cd43d938f
Capyvara13 <tiagorafleite@gmail.com>
2024-08-20 07:06:38 -0300

    Login password visibility ready

--- Login/LoginScript.js
+++ Login/LoginScript.js
@@ -1,10 +1,12 @@
 function mostrarSenha() {
   var senhaInput = document.getElementById("password");
   var tipoInput = senhaInput.getAttribute("type");
-
+  let ShowPassword = document.getElementById("ShowPassword");
   if (tipoInput === "password") {
     senhaInput.setAttribute("type", "text");
+    ShowPassword.style.backgroundImage = 'url("../img/Olho\ aberto.png")';
   } else {
     senhaInput.setAttribute("type", "password");
+    ShowPassword.style.backgroundImage = 'url("../img/Olho\ fechado.png")';
   }
 }
